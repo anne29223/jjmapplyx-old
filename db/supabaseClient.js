676ddcb4e044ab023c9b1a266e868ea8494360c6
anne29223 <221401538+anne29223@users.noreply.github.com>
2025-09-01 16:42:18 -0400

    fix errrors

--- db/supabaseClient.js
+++ db/supabaseClient.js
@@ -4,9 +4,3 @@ export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY
 );
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
-);
